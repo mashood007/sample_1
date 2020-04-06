a5393ef58f302f34b315c485fb021954e7163406
mashood <masoodpookkadan@gmail.com>
2020-04-06 20:12:04 +0530

    facebook auth while production

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -297,6 +297,6 @@ Devise.setup do |config|
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
 
-config.omniauth :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET'], scope: 'email'
+config.omniauth :facebook, "222493995525632", "620f3a6fe726f14d48b0a071f2ebba2e", scope: 'email'
 
 end
